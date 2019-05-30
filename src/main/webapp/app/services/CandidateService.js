8d7d2508fe269ed8895d77d08005bf54d106c00f
DimitrK <jim.feedback@gmail.com>
2019-05-30 22:33:26 +0300

    Enriched profile data

---
+++ src/main/webapp/app/services/CandidateService.js
@@ -0,0 +1,47 @@
+(function() {
+  "use strict";
+
+  angular.module("Matchmakers").factory("CandidateService", UserService);
+
+  UserService.$inject = ["$http", "$rootScope"];
+  function UserService($http, $rootScope) {
+    var service = {};
+    service.GetUrl = GetUrl;
+    service.Update = Update;
+    service.Read = Read;
+
+    return service;
+
+    function GetUrl(candidate) {
+      return (
+        $rootScope.backend_protocol +
+        "://" +
+        $rootScope.backend_ip +
+        ":" +
+        $rootScope.backend_port +
+        "/" +
+        $rootScope.backend_context_path +
+        "/candidates/" +
+        candidate
+      );
+    }
+
+    function Update({ token, ...candidateData }) {
+      debugger;
+      var config = {
+        headers: {
+          "Content-Type": "application/json;charset=utf-8;"
+        }
+      };
+      return $http.put(
+        service.GetUrl(token),
+        { id: token, ...candidateData },
+        config
+      );
+    }
+
+    function Read(user) {
+      return $http.get(service.GetUrl(user), { headers: headers });
+    }
+  }
+})();
