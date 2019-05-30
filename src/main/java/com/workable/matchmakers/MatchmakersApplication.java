e97a4402d7607a34e94afb6d81f2180adaab0a5d
Panos Antonakos <antonakos@workable.com>
2019-05-30 22:58:03 +0300

    RMV: Authentication

--- src/main/java/com/workable/matchmakers/MatchmakersApplication.java
+++ src/main/java/com/workable/matchmakers/MatchmakersApplication.java
@@ -3,14 +3,17 @@ package com.workable.matchmakers;
 import com.google.common.collect.ImmutableMap;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableScheduling
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 // @ComponentScan(basePackages = {"com.workable.matchmakers"})
@@ -33,5 +36,10 @@ public class MatchmakersApplication extends SpringBootServletInitializer {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // generates a 60char hash using random salt!
 	}
 }
