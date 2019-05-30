ba53905aab249904c8f8da82a3b15de12dd6e87b
DimitrK <jim.feedback@gmail.com>
2019-05-30 22:33:26 +0300

    Enriched profile data

--- src/main/webapp/app/components/profile/controllers/ProfileCtrl.js
+++ src/main/webapp/app/components/profile/controllers/ProfileCtrl.js
@@ -1,87 +1,113 @@
-(function () {
-	"use strict";
-	angular
-		.module("Matchmakers")
-		.controller("ProfileCtrl", ["$rootScope", "$scope", "$http", "$state", ProfileCtrl]);
+(function() {
+  "use strict";
+  angular
+    .module("Matchmakers")
+    .controller("ProfileCtrl", [
+      "CandidateService",
+      "$rootScope",
+      "$scope",
+      "$http",
+      "$state",
+      ProfileCtrl
+    ]);
 
-	function ProfileCtrl($rootScope, $scope, $http, $state) {
-        var ctrl = this;
-        ctrl.profileUrl = $rootScope.backend_protocol + "://" + $rootScope.backend_ip + ":" + $rootScope.backend_port + "/" + $rootScope.backend_context_path + "/candidates";
+  function ProfileCtrl(CandidateService, $rootScope, $scope, $http, $state) {
+    var ctrl = this;
+    ctrl.profileUrl =
+      $rootScope.backend_protocol +
+      "://" +
+      $rootScope.backend_ip +
+      ":" +
+      $rootScope.backend_port +
+      "/" +
+      $rootScope.backend_context_path +
+      "/candidates";
 
-        ctrl.init = function() {
-            $scope.candidate = {};
-            $scope.user = $rootScope.globals.currentUser || {};
-            $scope.initialModel = angular.copy($scope.candidate);
+    ctrl.init = function() {
+      $scope.candidate = {};
+      $scope.user = $rootScope.globals.currentUser || {};
+      $scope.objective = {};
+      $scope.initialModel = angular.copy($scope.candidate);
+    };
+
+    ctrl.reset = function() {
+      var message = "This will reset the form. Proceed anyway?";
+      $scope.modalWarning(message, "RESET").then(function(response) {
+        if (response === true) {
+          $scope.candidate = angular.copy($scope.initialModel);
+          // location.reload();
+          $scope.scrollTop();
         }
+      });
+    };
 
-        ctrl.reset = function() {
-            var message = "This will reset the form. Proceed anyway?";
-            $scope.modalWarning(message, "RESET")
-                .then(function (response) {
-                    if (response === true) {
-                        $scope.candidate = angular.copy($scope.initialModel);
-                        // location.reload();
-                        $scope.scrollTop();
-                    }
-                });
+    ctrl.cancel = function() {
+      var message = "Your work will be lost. Proceed anyway?";
+      $scope.modalWarning(message, "PROCEED").then(function(response) {
+        if (response === true) {
+          $state.go("jobs");
+          $scope.scrollTop();
         }
+      });
+    };
 
-        ctrl.cancel = function() {
-            var message = "Your work will be lost. Proceed anyway?";
-            $scope.modalWarning(message, "PROCEED")
-                .then(function (response) {
-                    if (response === true) {
-                        $state.go("jobs");
-                        $scope.scrollTop();
-                    }
-                });
+    ctrl.getFormCtrl = function() {
+      var retval = $scope.$$childHead;
+      if (retval) {
+        retval = retval.formCtrl;
+      }
+      return retval;
+    };
+
+    ctrl.isValid = function() {
+      var formCtrl = ctrl.getFormCtrl();
+      if (formCtrl && formCtrl.$valid) {
+        return true;
+      }
+
+      return false;
+    };
+
+    ctrl.add = function() {
+      var message =
+        "This will publish '" +
+        $scope.candidate.title +
+        "' to Matchmakers. Proceed?";
+      var requestData = {
+        ...$scope.user,
+        objective: {
+          roles: [$scope.objective.role],
+          locationsPrimary: [$scope.objective.locationsPrimary],
+          locationsSecondary: [$scope.objective.locationsSecondary]
         }
+      };
 
-        ctrl.getFormCtrl = function() {
-            var retval = $scope.$$childHead
-            if (retval) {
-                retval = retval.formCtrl;
+      $scope.modalWarning(message, "ADD").then(function(response) {
+        if (response === true) {
+          CandidateService.Update(requestData).then(
+            function successCallback(response) {
+              $scope.refreshJobs();
+              $state.go("jobs");
+              // Reload footer's img to switch from alert to check-mark!
+              $scope.createToast(
+                response.data.result + "! " + response.data.description
+              );
+              if ($rootScope.jobs === 0) {
+                location.reload();
+              } else {
+                $scope.scrollTop();
+              }
+            },
+            function errorCallback(response) {
+              $scope.createToast(
+                response.data.result + "! " + response.data.description
+              );
+              // var message = response.data.result + "<br/>" + response.data.description;
+              // $scope.modalError(message, "100");
             }
-            return retval;
+          );
         }
-
-        ctrl.isValid = function() {
-            var formCtrl = ctrl.getFormCtrl();
-            if (formCtrl && formCtrl.$valid) {
-                return true;
-            }
-
-            return false;
-        }
-
-        ctrl.add = function() {
-            var message = "This will publish '"+$scope.candidate.title +"' to Matchmakers. Proceed?";
-            var config = {
-                headers : {
-                    'Content-Type': 'application/json;charset=utf-8;'
-                },
-            }
-            $scope.modalWarning(message, "ADD")
-                .then(function (response) {
-                    if (response === true) {
-                        $http.put(ctrl.profileUrl, $scope.candidate, config)
-                            .then(function successCallback(response) {
-                                $scope.refreshJobs();
-                                $state.go("jobs");
-                                // Reload footer's img to switch from alert to check-mark!
-                                $scope.createToast(response.data.result + "! " + response.data.description)
-                                if ($rootScope.jobs === 0) {
-                                  location.reload();
-                                } else {
-                                  $scope.scrollTop();
-                                }
-                            }, function errorCallback(response) {
-                                $scope.createToast(response.data.result + "! " + response.data.description)
-                                // var message = response.data.result + "<br/>" + response.data.description;
-                                // $scope.modalError(message, "100");
-                            });
-                    }
-                });
-        }
-    }
-}());
+      });
+    };
+  }
+})();
