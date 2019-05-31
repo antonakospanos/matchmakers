66bb2021b02c162f638cf1eae1f3cc58d036497c
DimitrK <jim.feedback@gmail.com>
2019-05-31 21:30:00 +0300

    Updated mock data

--- src/main/webapp/app/components/jobs/controllers/JobsCtrl.js
+++ src/main/webapp/app/components/jobs/controllers/JobsCtrl.js
@@ -43,7 +43,7 @@
     // Initialization
     ctrl.init = function() {
       $scope.loading = Boolean($rootScope.globals.currentUser);
-      ctrl.listJobs();
+      ctrl.listJobsMock();
     };
 
     // Sorting states
@@ -224,9 +224,57 @@
      *  List Matchmakers jobs!
      */
     ctrl.listJobsMock = function() {
-      $scope.model = $http.defaults.headers.common.Authorization
+      var response = $http.defaults.headers.common.Authorization
         ? {
             data: [
+              {
+                logo: "https://workable-staging-workablestg5.s3.amazonaws.com/uploads/account/logo/591149101/small_logo",
+                company: "Lenovo",
+                title: "QA Automation Engineer",
+                description: "<p>We are looking for a Quality Assurance (QA) engineer to develop and execute exploratory and automated tests to ensure product quality. QA engineer responsibilities include designing and implementing tests, debugging and defining corrective actions. You will also review system requirements and track quality assurance metrics (e.g. defect densities and open defect counts.) The QA technician role plays an important part in our company’s product development process. Our ideal candidate will be responsible for conducting tests before product launches to ensure software runs smoothly and meets client needs, while being cost-effective. If you hold an engineering background and enjoy providing end-to-end solutions to software quality problems, we’d like to meet you. Ultimately, you should monitor all stages of software development to identify and resolve system malfunctions to meet quality standards.<strong>Responsibilities</strong></p><ul> <li>Review requirements, specifications and technical design documents to provide timely and meaningful feedback</li> <li>Create detailed, comprehensive and well-structured test plans and test cases</li> <li>Perform thorough regression testing when bugs are resolved</li> <li>Design, develop and execute automation scripts using open source tools</li> <li>Estimate, prioritize, plan and coordinate testing activities</li> <li>Liaise with internal teams (e.g. developers and product managers) to identify system requirements</li> <li>Investigate the causes of non-conforming software and train users to implement solutions</li> </ul>",
+                jobUrl: "https://lenovo.workablestg5.com/jobs/1073526216",
+                match: 90,
+                publisher: {
+                  name: "Lenovo",
+                  date: "31/5/2019"
+                }
+              },
+              {
+                logo: "https://workable-staging-workablestg5.s3.amazonaws.com/uploads/account/logo/1072641667/large_logo",
+                company: "Backbase",
+                title: "QA Tester",
+                description: "<p>We are looking for a QA Tester to assess software quality through manual and automated testing. You will be responsible for finding and reporting bugs and glitches. In this role, you should have a keen eye for detail and excellent communication skills. If you are also competent in executing test cases and are passionate about quality, we’d like to meet you. Ultimately, you will ensure that our products, applications and systems work correctly.<strong>Responsibilities</strong></p><ul> <li>Review and analyze system specifications</li> <li>Collaborate with QA Engineers to develop effective strategies and test plans</li> <li>Execute test cases (manual or automated) and analyze results</li> <li>Evaluate product code according to specifications</li> <li>Create logs to document testing phases and defects</li> <li>Report bugs and errors to development teams</li> <li>Help troubleshoot issues</li> <li>Conduct post-release/ post-implementation testing</li> <li>Work with cross-functional teams to ensure quality throughout the software development lifecycle</li> </ul>",
+                match: 85,
+                jobUrl: "https://backbase.workablestg5.com/jobs/1073526212",
+                publisher: {
+                  name: "Backbase",
+                  date: "31/5/2019"
+                }
+              },
+              {
+                logo: "https://media.licdn.com/dms/image/C560BAQGKY64CQns2jQ/company-logo_200_200/0?e=2159024400&v=beta&t=s1G1Yc9DxScmxM6ix9Pp3zb8lkBCAlKQi4-jXCUZQmk",
+                company: "Agile Actors",
+                title: "QA Engineer",
+                description: "<p>We are looking for a Quality Assurance (QA) engineer to develop and execute exploratory and automated tests to ensure product quality. QA engineer responsibilities include designing and implementing tests, debugging and defining corrective actions. You will also review system requirements and track quality assurance metrics (e.g. defect densities and open defect counts.) The QA technician role plays an important part in our company’s product development process. Our ideal candidate will be responsible for conducting tests before product launches to ensure software runs smoothly and meets client needs, while being cost-effective. If you hold an engineering background and enjoy providing end-to-end solutions to software quality problems, we’d like to meet you. Ultimately, you should monitor all stages of software development to identify and resolve system malfunctions to meet quality standards.<strong>Responsibilities</strong></p><ul> <li>Review requirements, specifications and technical design documents to provide timely and meaningful feedback</li> <li>Create detailed, comprehensive and well-structured test plans and test cases</li> <li>Estimate, prioritize, plan and coordinate testing activities</li> </ul>",
+                match: 85,
+                jobUrl: "https://agile-actors.workablestg5.com/jobs/1073526213",
+                publisher: {
+                  name: "AgileActors",
+                  date: "31/5/2019"
+                }
+              },
+              {
+                logo: "https://media.licdn.com/dms/image/C560BAQGKY64CQns2jQ/company-logo_200_200/0?e=2159024400&v=beta&t=s1G1Yc9DxScmxM6ix9Pp3zb8lkBCAlKQi4-jXCUZQmk",
+                company: "Agile Actors",
+                title: "Back-end Developer",
+                description: "<p>We are looking for an experienced Back-end developer to join our IT team. You will be responsible for the server side of our web applications. If you have excellent programming skills and a passion for developing applications or improving existing ones, we would like to meet you. As a Back-end developer, you’ll work closely with our engineers to ensure system consistency and improve user experience. Ultimately, you should be able to develop and maintain functional and stable web applications to meet our company’s needs.<strong>Responsibilities</strong></p><ul> <li>Participate in the entire application lifecycle, focusing on coding and debugging</li> <li>Troubleshoot and debug applications</li> <li>Perform UI tests to optimize performance</li> </ul>",
+                jobUrl: "https://agile-actors.workablestg5.com/jobs/1073526215",
+                match: 78,
+                publisher: {
+                  name: "AgileActors",
+                  date: "31/5/2019"
+                }
+              },
               {
                 logo: "https://alternative.me/icons/workable.jpg",
                 company: "Workable",
@@ -237,61 +285,18 @@
             
             Ultimately, the role of the Software Engineer is to build high-quality, innovative and fully performing software that complies with coding standards and technical design.`,
                 jobUrl: "https://careers.workable.com/j/9525118267?viewed=true",
-                match: 90,
+                match: 70,
                 publisher: {
                   name: "Workable",
                   date: "21/5/2019"
-                }
-              },
-              {
-                logo: "https://alternative.me/icons/workable.jpg",
-                company: "Workable",
-                title: "Test title",
-                description: "Test description",
-                jobUrl: "https://careers.workable.com/j/9525118267?viewed=true",
-                match: 90,
-
-                publisher: {
-                  name: "Workable",
-                  date: "21/5/2019"
-                }
-              },
-              {
-                logo: "https://alternative.me/icons/workable.jpg",
-                company: "Workable",
-                title: "Test title",
-                description: "Test description",
-                match: 80,
-                jobUrl: "https://workable.com",
-                publisher: {
-                  name: "Workable"
-                }
-              },
-              {
-                logo: "https://alternative.me/icons/workable.jpg",
-                company: "Workable",
-                title: "Test title",
-                description: "Test description",
-                jobUrl: "",
-                match: 78,
-                publisher: {
-                  name: "Workable"
-                }
-              },
-              {
-                logo: "https://alternative.me/icons/workable.jpg",
-                company: "Workable",
-                title: "Test title",
-                description: "Test description",
-                jobUrl: "",
-                match: 60,
-                publisher: {
-                  name: "Workable"
                 }
               }
             ]
           }
         : {};
+        $scope.model = {
+          data: response.data.map(d => ctrl.mapProgressModel(d))
+        };
     };
 
     /**
